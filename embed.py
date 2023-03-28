3c2e6016068f9d9e05e9e85564c2fad7896fe593
bonsaiskyline <93223150+bonsaiskyline@users.noreply.github.com>
2023-03-28 23:02:08 +0000

    Use pd.concat instead of append. Remove other embedding csv files.

--- embed.py
+++ embed.py
@@ -4,16 +4,21 @@ import pandas as pd
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-def get_embedding(text, model="text-embedding-ada-002"):
+def get_embedding(
+  text,
+  model="text-embedding-ada-002"
+  ):
    text = text.replace("\n", " ")
    return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
-df = pd.DataFrame()
+dfs = []
 for filename in os.listdir("./transcription"):
   if filename.endswith(".txt"):
-    with open("./transcription/" + filename, 'r') as f:
+    path = "./transcription/" + filename
+    video_name = filename.replace(".txt", "")
+    with open(path, 'r') as f:
       text = f.read()
       embedding = get_embedding(text, "text-embedding-ada-002")
-      embedding_name = filename.replace(".txt", "")
-      df = df.append({'name': embedding_name, 'embedding': embedding}, ignore_index=True)
-      df.to_csv('embedding/embedding-append.csv', index=False)
+      dfs.append(pd.DataFrame([[video_name, embedding]], columns=['video_name', 'embedding']))
+result_df = pd.concat(dfs, axis=0)
+result_df.to_csv('embedding/embedding.csv', index=False)

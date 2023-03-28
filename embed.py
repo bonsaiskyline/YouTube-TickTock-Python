be84f5c881e82810a8d328d3d9b0eda8c881de80
bonsaiskyline <93223150+bonsaiskyline@users.noreply.github.com>
2023-03-28 22:15:35 +0000

    Removed commented out lines

--- embed.py
+++ embed.py
@@ -15,19 +15,5 @@ for filename in os.listdir("./transcription"):
       text = f.read()
       embedding = get_embedding(text, "text-embedding-ada-002")
       embedding_name = filename.replace(".txt", "")
-      # embedding_filename = embedding_name + ".csv"
-      # df[embedding_name] = [embedding]
       df = df.append({'name': embedding_name, 'embedding': embedding}, ignore_index=True)
-      # df.to_csv('embedding/' + embedding_filename, index=False)
       df.to_csv('embedding/embedding-append.csv', index=False)
-
-#     with open("./embedding/{}.csv".format(name), 'w') as output:
-#       print(name)
-#       path = "./videos/" + filename
-#       transcript = transcribe(path)
-#       output.write(transcript.text)
-
-# text =
-# df = pd.read_csv('embedding/embedding.csv')
-# df['ada_embedding'] = df.combined.apply(lambda x: get_embedding(text, model='text-embedding-ada-002'))
-# df.to_csv('output/embedded_1k_reviews.csv', index=False)

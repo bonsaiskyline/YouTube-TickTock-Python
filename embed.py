a8aecebbe897a0092fad7f773ff89d21209b743e
bonsaiskyline <93223150+bonsaiskyline@users.noreply.github.com>
2023-03-28 22:11:47 +0000

    Add t-SNE and Kmeans notebooks

--- embed.py
+++ embed.py
@@ -15,8 +15,11 @@ for filename in os.listdir("./transcription"):
       text = f.read()
       embedding = get_embedding(text, "text-embedding-ada-002")
       embedding_name = filename.replace(".txt", "")
+      # embedding_filename = embedding_name + ".csv"
+      # df[embedding_name] = [embedding]
       df = df.append({'name': embedding_name, 'embedding': embedding}, ignore_index=True)
-df.to_csv('embedding/whole-transcription.csv', index=False)
+      # df.to_csv('embedding/' + embedding_filename, index=False)
+      df.to_csv('embedding/embedding-append.csv', index=False)
 
 #     with open("./embedding/{}.csv".format(name), 'w') as output:
 #       print(name)

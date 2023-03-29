714a570f4ce1e0251fd783bb2c44fd35c8bc877b
root <lingtongsun@gmail.com>
2023-03-30 01:33:59 +0200

    save

--- src/create-transcriptions.py
+++ src/create-transcriptions.py
@@ -1,47 +1,73 @@
 
 import os
-from .notebook-transcribe import group
+import re
+import json
+
+SPACERMILLI = 2000
+def millisec(timeStr):
+    spl = timeStr.split(":")
+    s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+    return s
+
+def group(filename):
+    dzs = open(filename).read().splitlines()
+    groups = []
+    g = []
+    lastend = 0
+
+    for d in dzs:
+        if g and (g[0].split()[-1] != d.split()[-1]):      #same speaker
+            groups.append(g)
+            g = []
+
+        g.append(d)
+
+        end = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=d)[1]
+        end = millisec(end)
+        if (lastend > end):       #segment engulfed by a previous segment
+            groups.append(g)
+            g = []
+        else:
+            lastend = end
+
+    if g:
+        groups.append(g)
+    print(*groups, sep='\n')
+    return groups
+
+def print_transcript(groups):
+    gidx = -1
+    transcript = ""
+    for g in groups:
+        gidx += 1
+        captions = json.load(open(str(gidx) + '.json'))['segments']
+
+        if captions:
+            speaker = g[0].split()[-1]
+            transcript += f"==> {speaker}: \n"
+
+            for c in captions:
+                for i, w in enumerate(c['words']):
+                    if w == "":
+                        continue
+                    transcript += w["word"]
+            transcript += '\n'
+    return transcript
 
 os.chdir('temp')
 current = os.getcwd()
 for folder_name in os.listdir(current):
     folder_path = os.path.join(current, folder_name)
     if os.path.isdir(folder_path):
+        os.chdir(folder_path)
+
         print(f"Folder found: {folder_name}")
+        if (folder_name == 'the_hot_box'):
+            continue
         output_file = "diarization.txt"
         groups = group(output_file)
-
-
-# for g in groups:
-#     shift = re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=g[0])[0]
-#     shift = millisec(shift) - SPACERMILLI #the start time in the original video
-#     shift=max(shift, 0)
-#     gidx += 1
-#     captions = json.load(open(str(gidx) + '.json'))['segments']
-
-#     if captions:
-#         speaker = g[0].split()[-1]
-#         boxclr = def_boxclr
-#         spkrclr = def_spkrclr
-#         if speaker in speakers:
-#             speaker, boxclr, spkrclr = speakers[speaker]
-
-#         html.append(f'<div class="e" style="background-color: {boxclr}">\n');
-#         html.append('<p  style="margin:0;padding: 5px 10px 10px 10px;word-wrap:normal;white-space:normal;">\n')
-#         html.append(f'<span style="color:{spkrclr};font-weight: bold;">{speaker}</span><br>\n\t\t\t\t')
-
-#         for c in captions:
-#             start = shift + c['start'] * 1000.0
-#             start = start / 1000.0   #time resolution ot youtube is Second.
-#             end = (shift + c['end'] * 1000.0) / 1000.0
-#             txt.append(f'[{timeStr(start)} --> {timeStr(end)}] [{speaker}] {c["text"]}\n')
-
-#             for i, w in enumerate(c['words']):
-#                 if w == "":
-#                     continue
-#                 start = (shift + w['start']*1000.0) / 1000.0
-#                 #end = (shift + w['end']) / 1000.0   #time resolution ot youtube is Second.
-#                 html.append(f'<a href="#{timeStr(start)}" id="{"{:.1f}".format(round(start*5)/5)}" class="lt" onclick="jumptoTime({int(start)}, this.id)">{w["word"]}</a><!--\n\t\t\t\t-->')
-#             #html.append('\n')
-#         html.append('</p>\n')
-#         html.append(f'</div>\n')
+        transcript = print_transcript(groups)
+        print(transcript)
+        with open('transcript.txt', 'w') as f:
+            f.write(transcript)
+        os.chdir('..')

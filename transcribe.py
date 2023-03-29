4cb13ef18c3ac5c0f4a05048aa22244f51781b8c
bonsaiskyline <93223150+bonsaiskyline@users.noreply.github.com>
2023-03-29 01:07:22 +0000

    Add dummy text files, refactor functions and fix lint warnings

--- transcribe.py
+++ transcribe.py
@@ -1,26 +1,32 @@
-import openai
+import openai_utils
 import os
 
-openai.api_key = os.environ['OPENAI_API_KEY']
-
-def transcribe(filename):
-  f = open(filename, 'rb')
-  transcript = openai.Audio.transcribe("whisper-1", f)
-  return transcript
 
 MAX_VIDEO_SIZE = 26214400
+VIDEO_PATH_PREFIX = "./video/"
+TRANSCIPTION_PATH_PREFIX = "./transcription/"
 
-for video_filename in os.listdir("./video"):
-  if video_filename.endswith(".mp4"):
-    video_path = "./video/" + video_filename
-    video_filesize = os.path.getsize(video_path)
-    if video_filesize >= 26214400:
-      print(video_filename + " is " + str(video_filesize) + " bytes. Max is " + str(MAX_VIDEO_SIZE) + " bytes.")
-      continue
-    transcription_filename = video_filename.replace('.mp4', '.txt')
-    transcription_path = "./transcription/" + transcription_filename
-    if not os.path.isfile(transcription_path):
-      with open(transcription_path, 'w') as output:
-        print(video_filename)
-        transcript = transcribe(video_path)
-        output.write(transcript.text)
+
+def transcribe_videos():
+    """
+    Transcribes videos.
+    """
+    for video_filename in os.listdir(VIDEO_PATH_PREFIX):
+        if video_filename.endswith(".mp4"):
+            video_path = VIDEO_PATH_PREFIX + video_filename
+            video_filesize = os.path.getsize(video_path)
+            if video_filesize >= MAX_VIDEO_SIZE:
+                print(video_filename + " is " + str(video_filesize) +
+                      " bytes. Max is " + str(MAX_VIDEO_SIZE) + " bytes.")
+                continue
+            transcription_filename = video_filename.replace('.mp4', '.txt')
+            transcription_path = TRANSCIPTION_PATH_PREFIX + transcription_filename
+            if not os.path.isfile(transcription_path):
+                with open(transcription_path, 'w') as output:
+                    print(video_filename)
+                    transcript = openai_utils.transcribe_audio(video_path)
+                    output.write(transcript.text)
+
+
+if __name__ == "__main__":
+  transcribe_videos()
